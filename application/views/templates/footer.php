f8a98735a7ce24b6e2f4cb87893e12ba09ce8e9b
10280588 <mennovleeuwen1990@gmail.com>
2013-04-16 07:19:40 -0400

    Spacing alignment is now done right

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,6 +4,6 @@
     <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
     <?php $phpLink = 'onzin' ?>
-<script type="text/javascript" src="<?php echo base_url() ?>js/trainer.js" ></script> 
+    <script type="text/javascript" src="<?php echo base_url() ?>js/trainer.js" ></script> 
 </body>
 </html>
