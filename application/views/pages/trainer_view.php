8065aa7c14390b7e8a6742ebafe279762b23bf65
10280588 <mennovleeuwen1990@gmail.com>
2013-04-16 13:26:27 -0400

    fixed indention and added a bootstrap control group for proper alignment of sum and response field

--- application/views/pages/trainer_view.php
+++ application/views/pages/trainer_view.php
@@ -8,30 +8,23 @@
     </div>
     <div class="row">
         <div class="span12">
-            
-                
-                    <form  class="form-horizontal">
-                    <div class="control-group">
-                        <label class="control-label" for="answer">
-                            <h1 class="sumSection">
-                                <span id ="sumText" >
-                                <?php echo $equation?>
-                                </span>
-                                <span>
-                                    =
-                                </span> 
-                            </h1>
-                        </label>
-                        <div class="controls">
-                            <input type="tel" id="answer" />
+            <form  class="form-horizontal">
+                <div class="control-group">
+                    <label class="control-label" for="answer">
+                        <div class="sumSection">
+                            <span id ="sumText" >
+                            <?php echo $equation?>
+                            </span>
+                            <span>
+                                =
+                            </span> 
                         </div>
+                    </label>
+                    <div class="controls">
+                        <input type="tel" id="answer" />
                     </div>
-                    </form>
-                    
-
-                
-           
-               
+                </div>
+            </form>               
         </div>
     </div>
     <div class="btn btn-large" id="submitAnswer">boe</div>
