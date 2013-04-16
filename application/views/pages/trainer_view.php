dff3fdac4a65a870f89e190d655da71cca790320
10280588 <mennovleeuwen1990@gmail.com>
2013-04-16 14:15:23 -0400

    moved the comment to the top of the screen

--- application/views/pages/trainer_view.php
+++ application/views/pages/trainer_view.php
@@ -1,6 +1,4 @@
-
-
-<div class="container">
+<div class="container sums">
     <div class="row">
         <div class="span12">
             <h1 class="appTitle" >Trainer </h1>
@@ -8,27 +6,36 @@
     </div>
     <div class="row">
         <div class="span12">
-            <form  class="form-horizontal">
-                <div class="control-group">
-                    <label class="control-label" for="answer">
-                        <div class="sumSection">
-                            <span id ="sumText" >
-                            <?php echo $equation?>
-                            </span>
-                            <span>
-                                =
-                            </span> 
-                        </div>
-                    </label>
-                    <div class="controls">
-                        <input type="tel" id="answer" />
-                    </div>
-                </div>
-            </form>               
+            <div id ="answerComment">
+            </div>
         </div>
     </div>
-    <div class="btn btn-large" id="submitAnswer">boe</div>
-    <div id ="answerComment">
-
+    <div class="row">
+        <div class="span12">
+            <form  class="form-horizontal" id="sumForm">
+            <div class="control-group">
+                <label class="control-label" >
+                    <div class="sumSection">
+                        <span id ="sumText" >
+                        <?php echo $equation?>
+                        </span>
+                        <span>
+                            =
+                        </span> 
+                    </div>
+                </label>
+                <div class="controls">
+                    <input type="tel" id="answer" />
+                </div>
+            </div>
+                           
+        </div>
+    </div>
+    <div class="row">
+        <div class="span12">
+            <button class="btn btn-large btn-warning" id="submitAnswer" type="button">Kijk na!</button>
+            </form><!-- end of sum form -->
+            
+        </div>
     </div>
 </div> <!-- /container -->
